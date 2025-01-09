9b570da7691e9d692ffa6cdc4d1ae34c0e483bbd
赵海堂 <java_findjob@163.com>
2025-01-09 21:37:28 +0800

    增加英雄面板功能

---
+++ mp/mp-skin.js
@@ -0,0 +1,113 @@
+
+var mpskin = {
+
+  InfoPanelTextFont: "Fonts\\\\ZITI1.ttf",
+  InfoPanelIconHeroIcon0: "ATTshuxing-str.webp",
+  InfoPanelIconHeroIcon1: "ATTshuxing-agi.webp",
+  InfoPanelIconHeroIcon2: "ATTshuxing-int.webp",
+
+  InfoPanelIconArmorDivine: "TYPElightdef.webp",
+  InfoPanelIconArmorHero: "TYPEdarkdef.webp",
+  InfoPanelIconArmorLarge: "TYPEwaterdef.webp",
+  InfoPanelIconArmorMedium: "TYPEfiredef.webp",
+  InfoPanelIconArmorNone: "TYPEstonedef.webp",
+  InfoPanelIconArmorSmall: "TYPEwinddef.webp",
+  InfoPanelIconDamageChaos: "TYPEwateratk.webp",
+  InfoPanelIconDamageHero: "TYPEdarkatk.webp",
+  InfoPanelIconDamageMagic: "TYPElightatk.webp",
+  InfoPanelIconDamageMelee: "TYPEstoneatk.webp",
+  InfoPanelIconDamageNormal: "TYPEstoneatk.webp",
+  InfoPanelIconDamagePierce: "TYPEwindatk.webp",
+  InfoPanelIconDamageSiege: "TYPEfireatk.webp",
+
+
+  ARMORTIP_DIVINE: "|cfffcd211法术抗性+20%|r|n来自其他类型攻击的伤害降低为|cff00f00080%|r|n来自|cffc552e6暗属性攻击|r的伤害提高为|cfff40000130%|r",
+  ARMORTIP_HERO: "|cfffcd211法术抗性+5%|r|n来自|cfff7f295光属性攻击|r的伤害提高为|cfff40000130%|r|n来自其他类型攻击的伤害为100%",
+  ARMORTIP_LARGE: "|cfffcd211法术抗性+15%|r|n来自|cffda400c火属性攻击|r的伤害降低为|cff00f00080%|r|n来自|cffea9f03土属性攻击|r和|cffc552e6暗属性攻击|r的伤害提高为|cfff40000120%|r|n来自其他类型攻击的伤害为100%",
+  ARMORTIP_MEDIUM: "|cfffcd211法术抗性+15%|r|n来自|cff78b58c风属性攻击|r的伤害降低为|cff00f00080%|r|n来自|cff73b4ea水属性攻击|r和|cffc552e6暗属性攻击|r的伤害提高为|cfff40000120%|r|n来自其他类型攻击的伤害为100%",
+  ARMORTIP_NONE: "|cfffcd211法术抗性+15%|r|n来自|cff73b4ea水属性攻击|r的伤害降低为|cff00f00080%|r|n来自|cff78b58c风属性攻击|r和|cffc552e6暗属性攻击|r的伤害提高为|cfff40000120%|r|n来自其他类型攻击的伤害为100%",
+  ARMORTIP_SMALL: "|cfffcd211法术抗性+15%|r|n来自|cffea9f03土属性攻击|r的伤害降低为|cff00f00080%|r|n来自|cffda400c火属性攻击|r和|cffc552e6暗属性攻击|r的伤害提高为|cfff40000120%|r|n来自其他类型攻击的伤害为100%",
+  ARMOR_DIVINE: "种类： |cfff7f295光属性护甲|r",
+  ARMOR_HERO: "种类： |cffc552e6暗属性护甲|r",
+  ARMOR_LARGE: "种类： |cff73b4ea水属性护甲|r",
+  ARMOR_MEDIUM: "种类： |cffda400c火属性护甲|r",
+  ARMOR_NONE: "种类： |cffea9f03土属性护甲|r",
+  ARMOR_SMALL: "种类： |cff78b58c风属性护甲|r",
+
+
+  DAMAGETIP_CHAOS: "对|cffda400c火属性护甲|r造成|cff00f000120%|r伤害|n对|cffea9f03土属性护甲|r和|cfff7f295光属性护甲|r造成|cfff4000080%|r伤害|n对其他类型护甲造成100%伤害",
+  DAMAGETIP_HERO: "对|cfff7f295光属性护甲|r造成|cff00f000130%|r伤害，对|cffc552e6暗属性护甲|r造成100%伤害，对其他类型护甲造成|cff00f000120%|r伤害",
+  DAMAGETIP_MAGIC: "对|cffc552e6暗属性护甲|r造成|cff00f000130%|r伤害|n对其他类型护甲造成100%伤害|n|cfff40000无法攻击魔法免疫的单位|r",
+  DAMAGETIP_MELEE: "对|cff73b4ea水属性护甲|r造成|cff00f000120%|r伤害|n对|cff78b58c风属性护甲|r和|cfff7f295光属性护甲|r造成|cfff4000080%|r伤害|n对其他类型护甲造成100%伤害",
+  DAMAGETIP_PIERCE: "对|cffea9f03土属性护甲|r造成|cff00f000120%|r伤害|n对|cffda400c火属性护甲|r和|cfff7f295光属性护甲|r造成|cfff4000080%|r伤害|n对其他类型护甲造成100%伤害",
+  DAMAGETIP_SIEGE: "对|cff78b58c风属性护甲|r造成|cff00f000120%|r伤害|n对|cff73b4ea水属性护甲|r和|cfff7f295光属性护甲|r造成|cfff4000080%|r伤害|n对其他类型护甲造成100%伤害",
+  DAMAGE_CHAOS: "种类： |cff73b4ea水属性攻击|r",
+  DAMAGE_HERO: "种类： |cffc552e6暗属性攻击|r",
+  DAMAGE_MAGIC: "种类： |cfff7f295光属性攻击|r",
+  DAMAGE_MELEE: "种类： |cffea9f03土属性攻击|r",
+  DAMAGE_NORMAL: "种类： |cff929292土属性攻击|r",
+  DAMAGE_PIERCE: "种类： |cff78b58c风属性攻击|r",
+  DAMAGE_SIEGE: "种类： |cffda400c火属性攻击|r",
+
+  INFOPANEL_LEVEL_CLASS: "|cfffcd211等级|r %u %s",
+  PRIMARY_ATTRIBUTE: "|cfffcd211 - 主属性|r|n - 每点增加2的攻击力|n",
+  COLON_DAMAGE: "|cfffcd211攻击力：|r",
+  COLON_ARMOR: "|cfffcd211护甲：|r",
+  COLON_HERO_ATTRIBUTES: "|cfffcd211英雄属性|r",
+  COLON_STRENGTH: "|cfff4606c力量：|r",
+  COLON_AGILITY: "|cff19caad敏捷：|r",
+  COLON_INTELLECT: "|cffbee7e9智力：|r",
+  COLON_STATUS: "|cfffcd211状态：|r",
+
+  str: " - 每点增加16的生命值|n - 每5点增加1的生命值恢复速度",
+  agi: " - 每20点增加1点的护甲|n - 每10点增加2%攻击速度",
+  int: " - 每点增加12的魔法值|n - 主属性为智力时，每100点提高0.4%法术穿透",
+  view: "视野：1500",
+  DefenseArmor: "0.02",
+
+
+  locArr: [
+    [0, 1903],
+    [0, 413, 585, 757, 956, 1159, 1347, 1523, 1903],
+    [0, 360, 545, 757, 956, 1159, 1347, 1583, 1903],
+    [0, 280, 545, 757, 956, 1159, 1407, 1623, 1903],
+    [0, 250, 495, 757, 956, 1159, 1437, 1693, 1903],
+    [0, 1903],
+  ],
+
+  heroArr: [
+    [''],
+    ['', 'O000', 'E011', 'E004', 'E008', 'E016', 'H00S', ''],
+    ['', 'O001', 'O003', 'E015', 'H006', 'H007', 'H00H', ''],
+    ['', 'O004', 'E01H', 'O00M', 'N06G', 'E014', 'H005', ''],
+    ['', 'N02D', '', '', 'O002', 'H00B', 'H00U', ''],
+    [''],
+  ],
+
+  heightArr: [0, 50, 201, 362, 552, 721, 1000],
+
+  heroImg: 'https://p3-sign.toutiaoimg.com/tos-cn-i-6w9my0ksvp/c97723ed8c8043e480d6790975363a61~tplv-tt-origin-web:gif.jpeg?_iz=58558&from=article.pc_detail&lk3s=953192f4&x-expires=1736609776&x-signature=MWDHPhmPkKRr03CnMKYgGJKUCZA%3D',
+  heroHeadImg: 'https://p3-sign.toutiaoimg.com/tos-cn-i-6w9my0ksvp/6b9aa6ae43454c2c9900ce22449ba447~tplv-obj.image?lk3s=993df49e&traceid=20250109212707F243562175C39FD419E1&x-expires=2147483647&x-signature=6ozQcOajxiJT0OpTGtdzmfrR1dw%3D',
+  imgSize: '730px 995px',
+  mapId: 192822,
+  calc(i,j,baseUnitId, unitId, p1, p2 ){
+    const basex = 172 * -0.7
+    const basey = 179 * -0.7
+    if (p1 != null && p1.unitId === unitId) {
+
+      console.log("定位头像位置：皮1")
+      offsetx = (j - 1) * basex
+      offsety = ((i - 1) * 2 + 1) * basey
+    } else if (p2 != null && p2.unitId === unitId) {
+      console.log("定位头像位置：皮2")
+      offsetx = (j - 1) * basex
+      offsety = ((i - 1) * 2 + 2) * basey
+    } else {
+      console.log("定位头像位置：原皮")
+      offsetx = (j - 1) * basex
+      offsety = (i - 1) * 2 * basey
+    }
+
+    return {offsetx, offsety}
+  }
+}
---
+++ x7/x7-skin.js
@@ -0,0 +1,107 @@
+var x7skin = {
+  InfoPanelIconArmorFort: "war3mapImported\\\\tYPEDeFend_Light.webp",
+  InfoPanelIconArmorHero: "war3mapImported\\\\tYPEDefDark.webp",
+  InfoPanelIconArmorLarge: "war3mapImported\\\\tYPEDefend_Land.webp",
+  InfoPanelIconArmorMedium: "war3mapImported\\\\tYPESystemPic_Defend_Wind.webp",
+  InfoPanelIconArmorNone: "war3mapImported\\\\tYPEDeFend_Water.webp",
+  InfoPanelIconArmorSmall: "war3mapImported\\\\tYPEDeFend_Fire.webp",
+  InfoPanelIconDamageChaos: "war3mapImported\\\\tYPEAttack_Fire.webp",
+  InfoPanelIconDamageHero: "war3mapImported\\\\tYPESystemPic_Attack_Dark.webp",
+  InfoPanelIconDamageMagic: "war3mapImported\\\\tYPEAttack_Light.webp",
+  InfoPanelIconDamageMelee: "war3mapImported\\\\tYPEAttack_Water.webp",
+  InfoPanelIconDamageNormal: "war3mapImported\\\\tYPEAttack_Water.webp",
+  InfoPanelIconDamagePierce: "war3mapImported\\\\tYPEAttack2_Feng.webp",
+  InfoPanelIconDamageSiege: "war3mapImported\\\\tYPEAttackLand.webp",
+
+  InfoPanelIconHeroIcon0: "UI\\\\Widgets\\\\Console\\\\Human\\\\infocard-heroattributes-str.webp",
+  InfoPanelIconHeroIcon1: "UI\\\\Widgets\\\\Console\\\\Human\\\\infocard-heroattributes-agi.webp",
+  InfoPanelIconHeroIcon2: "UI\\\\Widgets\\\\Console\\\\Human\\\\infocard-heroattributes-int.webp",
+
+  ARMORTIP_FORT: "|cffffffcc光属性护甲受到风水火地属性攻击伤害较少。|r|n|n|cff80ff80风属性攻击 - 80%|n水属性攻击 - 80%|n火属性攻击 - 80%|n地属性攻击 - 80%|n光属性攻击 - 80%|r|n|cffff0000暗属性攻击  - 130%",
+  ARMORTIP_HERO: "|cffcc99ff暗属性护甲会受到光属性攻击的极大伤害。|r|n|n风属性攻击 - 100%|n水属性攻击 - 100%|n火属性攻击 - 100%|n地属性攻击 - 100%|n|cffff0000光属性攻击 - 160%|n暗属性攻击 - 130%",
+  ARMORTIP_LARGE: "|cffffcc00地属性护甲受到水属性攻击伤害较少，但会受到风属性攻击的额外伤害。|r|n|n|cffff0000风属性攻击 - 130%|r|n|cff80ff80水属性攻击 - 70%|r|n火属性攻击 - 100%|n地属性攻击 - 100%",
+  ARMORTIP_MEDIUM: "|cffffcc00风属性护甲受地属性攻击伤害较少，但会受到火属性攻击的额外伤害。|r|n|n风属性攻击 - 100%|n水属性攻击 - 100%|n|cffff0000火属性攻击 - 130%|r|n|cff80ff80地属性攻击 - 70%",
+  ARMORTIP_NONE: "|cffffcc00水属性护甲受到火属性攻击伤害较少，但会受到地属性攻击的额外伤害。|r|n|n风属性攻击 - 100%|n水属性攻击 - 100%|n|cff80ff80火属性攻击 - 70%|r|n|cffff0000地属性攻击 - 130%|r",
+  ARMORTIP_NORMAL: "|cffffcc00风属性护甲受地属性攻击伤害较少，但会受到火属性攻击的额外伤害。|r|n|n风属性攻击 - 100%|n水属性攻击 - 100%|n|cffff0000火属性攻击 - 130%|r|n|cff80ff80地属性攻击 - 70%",
+  ARMORTIP_SMALL: "|cffffcc00火属性护甲受到风属性攻击伤害较少，但会受到水属性攻击的额外伤害。|r|n|n|cff80ff80风属性攻击 - 70%|r|n|cffff0000水属性攻击 - 130%|r|n火属性攻击 - 100%|n地属性攻击 - 100%",
+  ARMOR_FORT: "种类： |Cffffffcc光属性护甲|R",
+  ARMOR_HERO: "种类： |Cffcc99ff暗属性护甲|R",
+  ARMOR_LARGE: "种类： |Cffff9900地属性护甲|R",
+  ARMOR_MEDIUM: "种类： |Cff99cc00风属性护甲|R",
+  ARMOR_NONE: "种类： |Cff00ffff水属性护甲|R",
+  ARMOR_SMALL: "种类： |Cffff0000火属性护甲|R",
+  DAMAGETIP_CHAOS: "|cffffcc00火属性攻击对风属性护甲造成额外伤害，但对水属性护甲则会降低伤害。|r|n|n|cff80ff80风属性护甲 - 130%|r|n|cffff0000水属性护甲 - 70%|r|n火属性护甲 - 100%|n地属性护甲 - 100%",
+  DAMAGETIP_HERO: "|cffcc99ff暗属性攻击对所有属性护甲都会造成额外伤害。|r|n|n|cff80ff80风属性护甲 - 130%|n水属性护甲 - 130%|n火属性护甲 - 130%|n地属性护甲 - 130%|n光属性护甲 - 130%|n暗属性护甲 - 130%|n",
+  DAMAGETIP_MAGIC: "|cffffffcc光属性攻击对暗属性护甲造成极大的伤害。|r|n|n风属性护甲 - 100%|n水属性护甲 - 100%|n火属性护甲 - 100%|n地属性护甲 - 100%|n光属性护甲 - 100%|n|cff80ff80暗属性护甲 - 160%|r",
+  DAMAGETIP_MELEE: "|cffffcc00水属性攻击对火属性护甲造成额外伤害，但对地属性护甲则会降低伤害。|r|n|n风属性护甲 - 100%|n水属性护甲 - 100%|n|cff80ff80火属性护甲 - 130%|r|n|cffff0000地属性护甲 - 70%|r",
+  DAMAGETIP_NORMAL: "|cffffcc00水属性攻击对火属性护甲造成额外伤害，但对地属性护甲则会降低伤害。|r|n|n风属性护甲 - 100%|n水属性护甲 - 100%|n|cff80ff80火属性护甲 - 130%|r|n|cffff0000地属性护甲 - 70%|r",
+  DAMAGETIP_PIERCE: "|cffffcc00风属性攻击对地属性护甲造成额外伤害，但对火属性护甲则会降低伤害。|r|n|n风属性护甲 - 100%|n水属性护甲 - 100%|n|cffff0000火属性护甲 - 70%|r|n|cff80ff80地属性护甲 - 130%|r",
+  DAMAGETIP_SIEGE: "|cffffcc00地属性攻击对水属性护甲造成额外伤害，但对风属性护甲则会降低伤害。|r|n|n|cffff0000风属性护甲 - 70%|r|n|cff80ff80水属性护甲 - 130%|r|n火属性护甲 - 100%|r|n地属性护甲 - 100%",
+  DAMAGE_CHAOS: "种类： |Cffff0000火属性攻击|r",
+  DAMAGE_HERO: "种类： |Cffcc99ff暗属性攻击|r",
+  DAMAGE_MAGIC: "种类： |Cffffffcc光属性攻击|r",
+  DAMAGE_MELEE: "种类： |Cff00ffff水属性攻击|r",
+  DAMAGE_NORMAL: "种类： |Cff00ffff水属性攻击|r",
+  DAMAGE_PIERCE: "种类： |Cff99cc00风属性攻击|r",
+  DAMAGE_SIEGE: "种类： |Cffff9900地属性攻击|R",
+
+  INFOPANEL_LEVEL_CLASS:"等级|r %u %s",
+  PRIMARY_ATTRIBUTE:"|cfffcd211 - 主属性|r|n - 每点增加2的攻击力|n",
+  COLON_DAMAGE:"|cfffcd211攻击力：|r",
+  COLON_ARMOR:"|cfffcd211护甲：|r",
+  COLON_HERO_ATTRIBUTES:"|cfffcd211英雄属性|r",
+  COLON_STRENGTH:"|cfffcd211力量：|r",
+  COLON_AGILITY:"|cfffcd211敏捷：|r",
+  COLON_INTELLECT:"|cfffcd211智力：|r",
+  COLON_STATUS:"状态：|r",
+
+  str: " - 每点增加17的生命值|n - 每3点增加1的生命值恢复速度",
+  agi: " - 每16点增加1点的护甲|n - 每5点增加1%攻击速度",
+  int: " - 每点增加11的魔法值|n - 每20点增加1的魔法恢复速度",
+  view: "1600",
+  DefenseArmor :"0.03",
+  locArr : [
+    [0, 1903],
+    [0, 263, 416, 545, 717, 913, 1026, 1257, 1419, 1517, 1723, 1903],
+    [0, 179, 351, 529, 717, 913, 1050, 1257, 1425, 1594, 1757, 1903],
+    [0, 77, 323, 488, 717, 913, 1057, 1297, 1497, 1620, 1820, 1903],
+
+    [0, 405, 717, 1065, 1303, 1620, 1903],
+    [0, 1903],
+  ],
+
+  heroArr : [
+    [''],
+    ['', 'H00X', 'H00F', 'H003', 'H00D', 'E006', 'O005', 'H002', 'O001', 'O000', ''],
+    ['', 'U000', 'E003', 'E00Y', 'O003', 'H001', 'O002', 'N006', 'H005', 'H00E', ''],
+    ['', 'U001', 'H000', 'O00J', 'H006', 'E021', 'H00G', 'E00T', 'E00C', 'E029', ''],
+    ['', 'O010', '', 'E03O', 'O018', ''],
+    [''],
+  ],
+
+  heightArr : [0, 40, 216, 377, 567, 744, 1000],
+  heroImg : 'https://p3-sign.toutiaoimg.com/tos-cn-i-axegupay5k/994ee5ff602a4f74953ec88b4b4de6da~tplv-tt-origin-web:gif.jpeg?_iz=58558&from=article.pc_detail&lk3s=953192f4&x-expires=1736609776&x-signature=SDpnrLH%2FugNY8VDREqu5%2FuVhQAM%3D',
+  heroHeadImg: 'https://p3-sign.toutiaoimg.com/tos-cn-i-6w9my0ksvp/51fe0ffd02264cfcb7647d4da3a952ff~tplv-obj.image?lk3s=993df49e&traceid=20250109212707F243562175C39FD419E1&x-expires=2147483647&x-signature=d0foH6l4Wuf2mSRUz50za4yUD8M%3D',
+  imgSize: '1070px 1484px',
+  mapId : 193337,
+  calc(i,j,baseUnitId, unitId, p1, p2 ){
+    const basex = 169 * -0.7
+    const basey = 178 * -0.7
+    if (p1 != null && p1.unitId === unitId) {
+
+      console.log("定位头像位置：皮1")
+      offsetx = (j - 1) * basex
+      offsety = ((i - 1) * 3 + 1) * basey
+    } else if (p2 != null && p2.unitId === unitId) {
+      console.log("定位头像位置：皮2")
+      offsetx = (j - 1) * basex
+      offsety = ((i - 1) * 3 + 2) * basey
+    } else {
+      console.log("定位头像位置：原皮")
+      offsetx = (j - 1) * basex
+      offsety = (i - 1) * 3 * basey
+    }
+
+    return {offsetx, offsety}
+  }
+}
